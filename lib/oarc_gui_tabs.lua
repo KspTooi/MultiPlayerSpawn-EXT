5a17e3f482ac41e57a3166e11934ddf033cce575
ksptooi <ksptooi@protonmail.ch>
2021-02-22 23:12:40 +0800

    小幅重构了菜单相关的代码

--- lib/oarc_gui_tabs.lua
+++ lib/oarc_gui_tabs.lua
@@ -9,28 +9,110 @@ local mod_gui = require("mod-gui")
 -- NAME of the top level element (outer frame)
 OARC_GUI = "oarc_gui"
 
+
+-----------------BAD CODE-------------------!
+
 -- LIST of all implemented tabs and their content Functions
-OARC_GAME_OPTS_GUI_TAB_NAME = "Server Info"
-OARC_SPAWN_CTRL_GUI_NAME = "Spawn Controls"
-OARC_PLAYER_LIST_GUI_TAB_NAME = "Players"
-OARC_TAGS_GUI_TAB_NAME = "Name Tags"
-OARC_ROCKETS_GUI_TAB_NAME = "Rockets"
-OARC_SHARED_ITEMS_GUI_TAB_NAME = "Shared Items"
-OARC_NOTEPAD_GUI_TAB_NAME = "Notepad"
+OARC_GAME_OPTS_GUI_TAB_NAME = "服务器信息"
+OARC_SPAWN_CTRL_GUI_NAME = "我的基地"
+OARC_PLAYER_LIST_GUI_TAB_NAME = "在线玩家"
+OARC_TAGS_GUI_TAB_NAME = "我的标签"
+OARC_ROCKETS_GUI_TAB_NAME = "火箭相关"
+OARC_SHARED_ITEMS_GUI_TAB_NAME = "查看共享物品"
+OARC_NOTEPAD_GUI_TAB_NAME = "记事本"
 
-local OARC_GUI_TAB_CONTENT_FUNCTIONS = {}
-OARC_GUI_TAB_CONTENT_FUNCTIONS["Server Info"] = CreateGameOptionsTab
-OARC_GUI_TAB_CONTENT_FUNCTIONS["Spawn Controls"] = CreateSpawnCtrlGuiTab
-OARC_GUI_TAB_CONTENT_FUNCTIONS["Players"] = CreatePlayerListGuiTab
-OARC_GUI_TAB_CONTENT_FUNCTIONS["Name Tags"] = CreateTagGuiTab
-OARC_GUI_TAB_CONTENT_FUNCTIONS["Rockets"] = CreateRocketGuiTab
-OARC_GUI_TAB_CONTENT_FUNCTIONS["Shared Items"] = CreateSharedItemsGuiTab
-OARC_GUI_TAB_CONTENT_FUNCTIONS["Notepad"] = CreateNotepadGuiTab
+--[[local OARC_GUI_TAB_CONTENT_FUNCTIONS = {}
+OARC_GUI_TAB_CONTENT_FUNCTIONS["服务器信息"] = CreateGameOptionsTab
+OARC_GUI_TAB_CONTENT_FUNCTIONS["我的基地"] = CreateSpawnCtrlGuiTab
+OARC_GUI_TAB_CONTENT_FUNCTIONS["在线玩家"] = CreatePlayerListGuiTab
+OARC_GUI_TAB_CONTENT_FUNCTIONS["我的标签"] = CreateTagGuiTab
+OARC_GUI_TAB_CONTENT_FUNCTIONS["火箭相关"] = CreateRocketGuiTab
+OARC_GUI_TAB_CONTENT_FUNCTIONS["查看共享物品"] = CreateSharedItemsGuiTab
+OARC_GUI_TAB_CONTENT_FUNCTIONS["记事本"] = CreateNotepadGuiTab]]
 
+-----------------BAD CODE-------------------!
+
+local GUI_TAB_ITEM = {
+    {
+        itemName = "服务器信息",
+        itemValue = "",
+        itemTarget = CreateGameOptionsTab
+    },
+    {
+        itemName = "我的基地",
+        itemValue = "",
+        itemTarget = CreateSpawnCtrlGuiTab
+    },
+    {
+        itemName = "在线玩家",
+        itemValue = "playerList",
+        itemTarget = CreatePlayerListGuiTab
+    },
+    {
+        itemName = "我的标签",
+        itemValue = "playerTags",
+        itemTarget = CreateTagGuiTab
+    },
+    {
+        itemName = "火箭相关",
+        itemValue = "rocket",
+        itemTarget = CreateRocketGuiTab
+    },
+    {
+        itemName = "查看共享物品",
+        itemValue = "chestSharing",
+        itemTarget = CreateSharedItemsGuiTab
+    },
+    {
+        itemName = "记事本",
+        itemValue = "",
+        itemTarget = CreateNotepadGuiTab
+    }
+}
+
+
+
+--玩家菜单初始化
 function InitOarcGuiTabs(player)
+
     CreateOarcGuiButton(player)
 
-    -- Add general info tab
+    --遍历对象集合初始化玩家菜单
+    for i,k in pairs(GUI_TAB_ITEM)do
+
+        repeat
+
+
+            --如果当前循环到playerList 但配置文件中并没有开启这项功能 则continue循环
+            if global.ocfg.enable_player_list~=true and k.itemValue == "playerList" then
+                break
+            end
+
+            --如果当前循环到playerTags 但配置文件中并没有开启这项功能 则continue循环
+            if global.ocfg.enable_tags~=true and k.itemValue == "playerTags" then
+                break
+            end
+
+            --如果当前循环到rocket 但玩家并没有火箭 则不开启此按钮
+            if global.ocore.satellite_sent~=true and k.itemValue == "rocket" then
+                AddOarcGuiTab(player,k.itemName)
+                break
+            end
+
+            if global.ocfg.enable_chest_sharing~=true and k.itemValue == "chestSharing" then
+                break
+            end
+
+            AddOarcGuiTab(player,k.itemName)
+            SetOarcGuiTabEnabled(player,k.itemName,true)
+
+        until true
+
+    end
+
+    --------------BAD CODE-----------------
+
+--[[    -- Add general info tab
     AddOarcGuiTab(player, OARC_GAME_OPTS_GUI_TAB_NAME)
     SetOarcGuiTabEnabled(player, OARC_GAME_OPTS_GUI_TAB_NAME, true)
 
@@ -61,15 +143,17 @@ function InitOarcGuiTabs(player)
     end
 
     AddOarcGuiTab(player, OARC_NOTEPAD_GUI_TAB_NAME)
-    SetOarcGuiTabEnabled(player, OARC_NOTEPAD_GUI_TAB_NAME, true)
-    
+    SetOarcGuiTabEnabled(player, OARC_NOTEPAD_GUI_TAB_NAME, true)]]
+
+    --------------BAD CODE-----------------
+
     HideOarcGui(player)
 end
 
 function CreateOarcGuiButton(player)
     if (mod_gui.get_button_flow(player).oarc_button == nil) then
         local b = mod_gui.get_button_flow(player).add{name="oarc_button",
-                                                        caption="CLICK ME FOR MORE INFO",
+                                                        caption="点此打开菜单",
                                                         type="sprite-button",
                                                         -- sprite="utility/expand_dots",
                                                         style=mod_gui.button_style}
@@ -102,11 +186,13 @@ function HideOarcGui(player)
 end
 
 function GetOarcGuiTabsPane(player)
+
     if (mod_gui.get_frame_flow(player)[OARC_GUI] == nil) then
         return nil
     else
         return mod_gui.get_frame_flow(player)[OARC_GUI].oarc_if.oarc_tabs
     end
+
 end
 
 function ClickOarcGuiButton(event)
@@ -236,15 +322,38 @@ function AddOarcGuiTab(player, tab_name)
 end
 
 
+--渲染玩家GUI内容的方法
+--player 玩家实例
+--player tab_name 当前选择的TAB标签
 function SetOarGuiTabContent(player, tab_name)
+
     if (not DoesOarcGuiExist(player)) then return end
 
+    --获取所有可用的标签实例
     local otabs = GetOarcGuiTabsPane(player)
 
+
+    --遍历该玩家的所有TAB标签
     for _,t in ipairs(otabs.tabs) do
+
+        --如果该玩家的所有标签里包含选中的标签
         if (t.tab.name == tab_name) then
+
+            --清除旧内容
             t.content.clear()
-            OARC_GUI_TAB_CONTENT_FUNCTIONS[tab_name](t.content, player)
+
+            --遍历对象集合
+            for i,k in ipairs(GUI_TAB_ITEM) do
+
+                if(k.itemName == tab_name) then
+                    k.itemTarget(t.content, player)
+                end
+
+            end
+
+            --渲染Tab内容
+            --OARC_GUI_TAB_CONTENT_FUNCTIONS[tab_name](t.content, player)
+
             return
         end
     end
